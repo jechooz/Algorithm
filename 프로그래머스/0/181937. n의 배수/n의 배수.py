5cb528965a00e311907d4be490fd4ed8538949c1
cj <126685949+jechooz@users.noreply.github.com>
2024-07-14 14:04:40 +0900

    Update n의 배수.py

--- 프로그래머스/0/181937. n의 배수/n의 배수.py
+++ 프로그래머스/0/181937. n의 배수/n의 배수.py
@@ -1,2 +1,12 @@
 def solution(num, n):
     return 1 if num%n==0 else 0
+
+
+```다른 풀이
+def solution(num, n):
+    return int(num % n == 0)
+
+num % n == 0은 num이 n의 배수일 때 참(True)이고, 그렇지 않을 때 거짓(False)
+int() 함수를 사용하여 참(True)일 때 1, 거짓(False)일 때 0을 반환
+
+```
