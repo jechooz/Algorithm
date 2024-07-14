cc5042973d4b7e4fa36056da86fcba05c087ca7a
cj <126685949+jechooz@users.noreply.github.com>
2024-07-14 14:34:55 +0900

    Update 홀짝에 따라 다른 값 반환하기.py

--- 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.py
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.py
@@ -11,4 +11,15 @@ def solution(n):
                 
     return answer            
                 
-            
+    '''다른 풀이
+    def solution(n):
+    if n % 2:  # n이 홀수일 경우
+        return sum(range(1, n+1, 2))  # n 이하의 홀수들의 합을 반환
+    else:  # n이 짝수일 경우
+        return sum([i*i for i in range(2, n+1, 2)])  # n 이하의 짝수들의 제곱의 합을 반환
+
+
+   n % 2가 0이 아닌 값 (즉, 1이나 -1 등) 일 경우, 조건식은 참(True)으로 간주.이는 n이 홀수일 때와 같은 결과
+   n % 2가 0일 경우, 조건식은 거짓(False)으로 간주됩니다. 이는 n이 짝수인 경우와 같은 결과
+
+    '''
