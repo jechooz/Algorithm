ef39d3f5ad705c9c027a181551b9ec9dae78ef8c
cj <126685949+jechooz@users.noreply.github.com>
2024-07-15 15:34:58 +0900

    Update 주사위 게임 2.py

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
@@ -1,3 +1,5 @@
+###내 풀이
+
 def solution(a, b, c):
     answer = 0
     
@@ -9,3 +11,16 @@ def solution(a, b, c):
         answer = (a + b + c) * (a**2 + b**2 + c**2)
         
     return answer
+
+```다른 방법 풀이
+
+def solution(a, b, c):
+    check=len(set([a,b,c]))
+    if check==1:
+        return 3*a*3*(a**2)*3*(a**3)
+    elif check==2:
+        return (a+b+c)*(a**2+b**2+c**2)
+    else:
+        return (a+b+c)
+
+```
