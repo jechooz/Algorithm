7f74f59700828de43215857952189fcebe08d55a
cj <126685949+jechooz@users.noreply.github.com>
2024-08-14 15:49:36 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.py
@@ -0,0 +1,12 @@
+def solution(n):
+    
+    #Str(), resverse() map() int() 활용
+    
+    #1.정수를 스트링변경 리스트묶음
+    arr=list(str(n))
+    
+    #2.리버스
+    arr.reverse()
+    
+    #3.정수로 변경
+    return list(map(int,arr))
