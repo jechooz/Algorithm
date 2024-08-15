6e9c12cc42a1550d705f5181f84f9c2b17a8938d
cj <126685949+jechooz@users.noreply.github.com>
2024-08-15 15:18:11 +0900

    Update 하샤드 수.py

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.py
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.py
@@ -5,3 +5,17 @@ def solution(x):
     xplus = sum(int(i) for i in arr)
     
     return x%xplus==0
+
+
+```python 다른풀이
+
+def sum_of_integers_between(start, end):
+    # start가 end보다 크면 두 값을 바꿈
+    if start > end:
+        start, end = end, start
+    
+    # range 함수는 start부터 end까지의 정수 범위를 생성.
+    # end + 1을 사용하는 이유는 range 함수가 end 값을 포함하지 않기때문.
+    return sum(range(start, end + 1))
+
+```
