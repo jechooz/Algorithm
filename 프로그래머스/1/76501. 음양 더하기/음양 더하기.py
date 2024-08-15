71773427577dc1a43698159b74e5e90d0bca8c81
cj <126685949+jechooz@users.noreply.github.com>
2024-08-15 16:30:57 +0900

    Update 음양 더하기.py

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
@@ -8,3 +8,13 @@ def solution(absolutes, signs):
             answer-=absolutes[i]
         
     return answer
+
+# 다른 풀이    
+# def solution(absolutes, signs):
+#     answer=0
+#     for absolute,sign in zip(absolutes,signs):
+#         if sign:
+#             answer+=absolute
+#         else:
+#             answer-=absolute
+#     return answer
