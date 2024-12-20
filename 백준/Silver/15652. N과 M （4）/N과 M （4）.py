68e9bdf78078688b82cf50db87dcd250b5dd5874
cj <126685949+jechooz@users.noreply.github.com>
2024-12-21 05:27:32 +0900

    [Silver III] Title: N과 M (4), Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+result = []
+
+def solution(level, start):
+    if level == M:
+        print(' '.join(map(str, result)))
+        return
+    for i in range(start, N + 1): 
+        result.append(i)  
+        solution(level + 1, i)  
+        result.pop()  
+
+solution(0, 1)
