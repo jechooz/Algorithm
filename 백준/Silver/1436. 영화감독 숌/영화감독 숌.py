c201179edb45d3f8339e9a41e56e1742756072e1
cj <126685949+jechooz@users.noreply.github.com>
2025-01-07 18:48:24 +0900

    [Silver V] Title: 영화감독 숌, Time: 704 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+number = 666
+answer = 666
+while N-1:
+    number +=1
+    
+    if '666' in str(number):
+        answer = number
+        N -=1
+    
+    
+    
+print(number)
+            
